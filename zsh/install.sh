7581b6cc90a05011d919dc6dfdf4dcf12dbee7b2
pickline <adcmasters@outlook.com>
2022-12-08 13:32:11 +0800

    change bat's theme

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,12 +2,26 @@ SYSTEM=`uname -s`
 echo $SYSTEM
 
 # some dependencies
-if [ 'Darwin' = $SYSTEM ] then
+if [ 'Darwin' = $SYSTEM ]; then
     brew install bat
     brew install neovim --HEAD
-elif [ 'Linux' = $SYSTEM ] then
+    brew install dust
+    brew install duf
+    brew install ack
+    brew install htop
+    brew install procs
+elif [ 'Linux' = $SYSTEM ]; then
     sudo apt install bat
     sudo apt install neovim
+    sudo apt install htop
+
+    sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
+    pacstall -I dust-bin
+    pacstall -I duf-deb
+
+    curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
+    source "$HOME/.cargo/env"
+    cargo install procs
 fi
 
 # install zimfw
