73c15af0e081a4b7178cdb39eade18f2ae741cf2
pickline <adcmasters@outlook.com>
2022-12-08 13:42:42 +0800

    install.sh changes

--- install.sh
+++ install.sh
@@ -1,17 +1,35 @@
 SYSTEM=`uname -a`
 
 if [ -d '~/.config/nvim' ] then
-    rm -rf ~/.config.nvim
+    rm -rf ~/.config/nvim
 fi
-
 if [ -d '~/.vim' ] then
     rm -rf ~/.vim 
 fi
 
-if [ 'Darwin' = $SYSTEM ] then
+# some dependencies
+if [ 'Darwin' = $SYSTEM ]; then
     brew install neovim --HEAD
     brew install lazygit
-elif [ 'Linux' = $SYSTEM ] then
+    brew install bat
+    brew install dust
+    brew install duf
+    brew install ack
+    brew install htop
+    brew install procs
+elif [ 'Linux' = $SYSTEM ]; then
+    sudo apt install bat
+    sudo apt install neovim
+    sudo apt install htop
+
+    sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
+    pacstall -I dust-bin
+    pacstall -I duf-deb
+
+    curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
+    source "$HOME/.cargo/env"
+    cargo install procs
+
     # neovim
     sudo add-apt-repository ppa:neovim-ppa/stable
     sudo apt update
@@ -22,6 +40,15 @@ elif [ 'Linux' = $SYSTEM ] then
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
 fi
+
+# install zimfw
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+# use custom config file
+cp zsh/initrc ~/.initrc
+cp zsh/zshrc ~/.zshrc
+cp zsh/zimrc ~/.zimrc
+# install for zimfw
+zimfw install
 
 # install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
